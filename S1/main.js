18d860b3df00ee49e52adf13a7d360ef0980db31
Pawan Yadav <ypawan83@outlook.com>
2023-03-16 12:32:49 +0530

    chapter1

--- S1/main.js
+++ S1/main.js
@@ -1,6 +1,23 @@
-const heading = React.createElement("h1", {}, "Welcome to React");
+const heading1 = React.createElement("h1", {
+    id: "heading1",
+    class: "heading"
+}, "Welcome to React");
+
+const heading2 = React.createElement("h2",{
+    id: "heading2",
+    className: "heading"
+},"React is a Javascript Library");
+
+console.log(heading1);
+
+const container = React.createElement("div",{
+    id: "container",
+    className: "outer"
+},[heading1,heading2]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// render adds the element to the root in the dom tree 
+
+root.render(container);
 
