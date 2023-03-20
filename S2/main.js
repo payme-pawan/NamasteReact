1c655ec2560f690466745a80702c8a7ad6ae1e75
Pawan Yadav <ypawan83@outlook.com>
2023-03-20 11:55:18 +0530

    S2 complete

---
+++ S2/main.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading1 = React.createElement("h1", {
+    id: "heading1",
+    class: "heading"
+}, "Welcome to React");
+
+const heading2 = React.createElement("h2",{
+    id: "heading2",
+    className: "heading"
+},"React is a Javascript Library");
+
+console.log(heading1);
+
+const container = React.createElement("div",{
+    id: "container",
+    className: "outer"
+},[heading1,heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// render adds the element to the root in the dom tree 
+
+root.render(container);
